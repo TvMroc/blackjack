56c222f3dbe119059b539c46b9165075f30e18b8
TvMroc <1207837@student.roc-nijmegen.nl>
2025-02-19 08:43:19 +0100

    button 1 event
    button 1 click event test

--- test/Form1.cs
+++ test/Form1.cs
@@ -6,5 +6,10 @@ namespace test
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("BUTTON clicked");
+        }
     }
 }
